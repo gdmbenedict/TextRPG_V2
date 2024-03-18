da50d8d5e2de91a6388e7970643b34ff34d1c3cd
Matthieu Benedict <97464794+Twitchton@users.noreply.github.com>
2024-03-18 10:08:50 -0300

    Item Added
    Added an Item abstract class.

---
+++ TextRPG_V2/Items/Item.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_V2
+{
+    internal abstract class Item
+    {
+        private string name; //name of the item
+
+        /// <summary>
+        /// Constructor method for an abstract item object
+        /// </summary>
+        /// <param name="name">name of the item</param>
+        public Item(string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Empty Constructor for an Item object
+        /// </summary>
+        public Item()
+        {
+            name = null;
+        }
+
+        /// <summary>
+        /// Abstract method for using an item
+        /// </summary>
+        /// <param name="target">The entity that is using the item</param>
+        /// <returns>A string for the event log</returns>
+        public string Use(Entity target)
+        {
+            string useMessage;
+            useMessage = target.GetName() + " used a " + name;
+
+            return useMessage;
+        }
+
+        /// <summary>
+        /// Accessor method for the name of the item
+        /// </summary>
+        /// <returns>name of the item</returns>
+        public string GetName()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// Mutator
+        /// </summary>
+        /// <param name="name"></param>
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+
+    }
+}
