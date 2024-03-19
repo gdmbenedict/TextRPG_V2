745dce8dfb94b3d002c4f652e379239c0f98f273
Matthieu Benedict <97464794+Twitchton@users.noreply.github.com>
2024-03-19 12:05:53 -0300

    Map Initialization Structure
    General Map initialization started, still needs to take in enemies and items from the loading file.

--- TextRPG_V2/Maps/Map.cs
+++ TextRPG_V2/Maps/Map.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TextRPG_V2;
+using System.IO;
 
 namespace TextRPG_V2
 {
@@ -13,11 +14,67 @@ namespace TextRPG_V2
         private Tile[,] background;
         private Entity[,] entities;
         private Item[,] items;
+        private int height;
+        private int width;
 
         public Map(string path)
         {
+            //declaring some variables (default values added to handle no value error)
+            int startIndex = 0, endIndex = 0;
 
+            //check if file exists
+            if (!File.Exists(@path))
+            {
+                //throw an error
+            }
+
+            //getting input from file
+            string[] input = File.ReadAllLines(@path);
+
+            //parsing through file for basic information
+            //this section needs to be changed to fit JSON file format later
+            mapName = input[0];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == "{")
+                {
+                    startIndex = i + 1; //sets start of map below open bracket
+                }
+
+                if (input[i] == "}")
+                {
+                    endIndex = i - 1; //sets end of map above closed bracket
+                    break;
+                }
+            }
+
+            if (!(endIndex >= startIndex))
+            {
+                //throw error
+            }
+
+            height = endIndex - startIndex + 1; //gets height of map
+            width = input[startIndex].Length; //gets width of map
+
+            //initializing Tile map, Item map, & Entity map
+            background = new Tile[height, width];
+            entities = new Entity[height, width];
+            items = new Item[height, width];
+
+            //cycle through every char of the input map
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    //sets each individual tile to their correct value
+                    background[y, x] = new Tile(input[y + startIndex][x]);
+                }
+
+            }
+
+            //lead in items and enemies
         }
     }
-
 }
+
--- TextRPG_V2/SharedEnums.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace TextRPG_V2
-{
-    
-}
