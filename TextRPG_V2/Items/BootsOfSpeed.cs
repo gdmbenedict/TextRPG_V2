935c832254a5535815c962fd5174d162aff4e80d
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-03-20 16:38:53 -0300

    Added Boots of Speed Item

---
+++ TextRPG_V2/Items/BootsOfSpeed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_V2
+{
+    internal class BootsOfSpeed : Item
+    {
+
+        public BootsOfSpeed() : base()
+        {
+
+        }
+
+        new public string Use(Entity target)
+        {
+            string message = base.Use(target);
+            message += " and maxed their speed";
+
+            target.spd.SetStat(target.spd.GetMaxStat());
+
+            return message;
+        }
+    }
+}
